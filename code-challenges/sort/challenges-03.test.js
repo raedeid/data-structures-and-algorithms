24a7b2afb742deba6194709549ac760d2071e2ad
raedeid <raededialrfou12345@gmail.com>
2019-12-20 17:47:12 +0200

    do some stuff

--- code-challenges/sort/challenges-03.test.js
+++ code-challenges/sort/challenges-03.test.js
@@ -7,7 +7,7 @@ Write a function named sortBackwards that takes in an array of numbers and retur
 ------------------------------------------------------------------------------------------------ */
 
 const sortBackwards = (arr) => {
-  // Solution code here...
+  arr.sort()
 };
 
 /* ------------------------------------------------------------------------------------------------
